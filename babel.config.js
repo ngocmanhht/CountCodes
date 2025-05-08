6f2047092acc48141b5450f386e1b15cff85d01a
Anh Ngoc <ngocda@ows.com.vn>
2025-05-09 00:30:04 +0700

    ✨ Update project configuration and dependencies for improved functionality and performance

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,21 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    'react-native-reanimated/plugin',
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        // alias: {
+        //   '@components': './src/components',
+        //   '@assets': './src/assets',
+        //   '@screens': './src/screens',
+        //   '@utils': './src/utils',
+        //   '@hooks': './src/hooks',
+        //   '@context': './src/context',
+        // },
+      },
+    ],
+  ],
 };
