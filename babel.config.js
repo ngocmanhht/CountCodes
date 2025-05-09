676e013734c9d6d44693b650919b9cf3ff017d8e
ngocda <ngocda@newwave.com.vn>
2025-05-09 15:44:16 +0700

    Implement structural updates and optimizations across multiple modules

--- babel.config.js
+++ babel.config.js
@@ -1,21 +1,4 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: [
-    'react-native-reanimated/plugin',
-    [
-      'module-resolver',
-      {
-        root: ['./src'],
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-        // alias: {
-        //   '@components': './src/components',
-        //   '@assets': './src/assets',
-        //   '@screens': './src/screens',
-        //   '@utils': './src/utils',
-        //   '@hooks': './src/hooks',
-        //   '@context': './src/context',
-        // },
-      },
-    ],
-  ],
+  plugins: [['react-native-worklets-core/plugin']],
 };
